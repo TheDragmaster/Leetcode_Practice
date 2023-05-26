45c5d7f0ee76722d0991832b0451979439f693a3
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-05-26 18:34:59 -0500

    Add files via upload

---
+++ Longest_Substring.py
@@ -0,0 +1,14 @@
+#Make a set for the substring to make sure you do not end up with a duplicate 
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0       #Initialize the left pointer 
+        res = 0
+
+        for r in range (len(s)):            #Initialize the right pointer 
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
