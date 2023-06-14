baea747dbb27a9a3d2ad16b4921f66206ea270b9
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-06-13 20:10:49 -0500

    Update Leetcode_169.py

--- Leetcode_169.py
+++ Leetcode_169.py
@@ -1,13 +1,25 @@
 # We are using a hashmap because we can use any key - value that we want
 
 
+# class Solution:
+# def majorityElement(self, nums: List[int]) -> int:
+#   count = {}
+#   res, maxCount = 0, 0
+#
+#   for n in nums:
+#      count[n] = 1 + count.get(n, 0)
+#     res = n if count[n] > maxCount else res
+#    maxCount = max(count[n], maxCount)
+# return res
+# Solution using boyer_Moore algorithm/ linear time and in 0(1) space
+
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = {}
-        res, maxCount = 0, 0
+        res, count = 0, 0
 
         for n in nums:
-            count[n] = 1 + count.get(n, 0)
-            res = n if count[n] > maxCount else res
-            maxCount = max(count[n], maxCount)
+            if count == 0:
+                res = n
+            count += 1 if n == res else -1
         return res
