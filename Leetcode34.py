2227138edc6c464a4559e4796d0f0ae79bd5ae0c
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-06-19 00:27:46 -0500

    Add files via upload

---
+++ Leetcode34.py
@@ -0,0 +1,26 @@
+# We will be using binary search for this problem
+# For Binary search you initialize a Left most pointer and Right most pointer in the array then do L + R / 2 to initialize the middle pointer
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = self.binSearch(nums, target, True)
+        right = self.binSearch(nums, target, False)
+        return [left, right]
+
+    def binSearch(self, nums, target, leftBias):
+        l, r = 0, len(nums) - 1
+        i = -1
+        while l <= r:
+            m = (l + r) // 2
+            if target > nums[m]:
+                l = m + 1
+            elif target < nums[m]:
+                r = m - 1
+            else:
+                i = m
+                if leftBias:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return i
