44ad78b3c57df02aa515d8d78f2e8327c873e621
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-07-26 20:15:19 -0500

    Update Leetcode70.py

--- Leetcode70.py
+++ Leetcode70.py
@@ -1,3 +1,5 @@
+# Using dynamic programming to simplify the problem as much as possible, We only need to keep track of 2 numbers and start from the end value to computer the 
+# End solution 
 class Solution:
     def climbStairs(self, n: int) -> int:
         one, two = 1, 1
