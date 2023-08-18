1cfccf6c4d99d7397325a92c1a19f3b0847e4cae
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-08-17 22:54:57 -0500

    Update Leetcode200.py

--- Leetcode200.py
+++ Leetcode200.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution:                        #Leetcode 200 Number Of Island
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
