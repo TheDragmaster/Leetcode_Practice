b90909f0ffcc2913f4ca64818e19b9b3f4b6ec59
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-09-07 12:37:10 -0500

    Add files via upload

---
+++ Leetcode124.py
@@ -0,0 +1,27 @@
+#Leetcode 124 binary tree maximum path sum
+#Travel through a path on the binary tree and get the maximum sum valued and return the max possible sum within the tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+
+        def dfs(root):
+            if not root:
+                return 0
+            
+            # return max path sum without spliting paths
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+
+            return root.val + max(leftMax, rightMax)
+        dfs(root)
+        return res[0]
